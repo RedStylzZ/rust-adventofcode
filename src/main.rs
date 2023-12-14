8cfea1716273f16cf07b078c9d98209d1dcc4b2d
Tizian <tizianappler@yahoo.de>
2023-12-14 22:41:52 +0100

    Solved day 1

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,93 @@
 use std::fs;
 
-fn numbers(line: &str) -> Vec<i32> {
-    let mut v: Vec<i32> = Vec::new();
+fn get_numbers(line: &str) -> Vec<u32> {
+    let mut v: Vec<u32> = Vec::new();
 
-    let mut buf: String = String::new();
     for c in line.chars() {
         if c >= '0' && c <= '9' {
-            buf.push(c);
+            v.push(c.to_digit(10).unwrap());
+        }
+    }
+
+    return v;
+}
+
+fn part_one(lines: &Vec<&str>) -> u32 {
+    let mut sum: u32 = 0;
+    for line in lines {
+        let numbers = get_numbers(line);
+        let num: u32 = format!("{}{}", numbers[0], numbers[numbers.len() - 1])
+            .parse()
+            .unwrap();
+        sum += num;
+    }
+
+    sum
+}
+
+fn str_to_int(s: &str) -> u32 {
+    return match s {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => 0,
+    };
+}
+
+fn get_numbers_part_two(line: &str) -> Vec<u32> {
+    let chars = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut v: Vec<u32> = Vec::new();
+
+    for (i, c) in line.chars().enumerate() {
+        if c >= '0' && c <= '9' {
+            v.push(c.to_digit(10).unwrap());
             continue;
         }
 
-        if buf.len() > 0 {
-            let i = buf.parse().unwrap();
-            v.push(i);
-            buf.clear()
+        for num in chars {
+            if i + num.len() > line.len() {
+                continue;
+            }
+
+            let s = &line[i..i + num.len()];
+
+            if s == num {
+                let x = str_to_int(s);
+                v.push(x);
+                break;
+            }
         }
     }
 
-    if buf.len() > 0 {
-        let i = buf.parse().unwrap();
-        v.push(i);
+    return v;
+}
+
+fn part_two(lines: &Vec<&str>) -> u32 {
+    let mut sum: u32 = 0;
+
+    for line in lines {
+        let parts = get_numbers_part_two(line);
+
+        let num: u32 = format!("{}{}", parts[0], parts[parts.len() - 1])
+            .parse()
+            .unwrap();
+
+        sum += num;
     }
 
-    return v
+    sum
 }
 
 fn main() {
-
-    let file_path = "input/day06_test.txt";
+    let file_path = "input/day01.txt";
 
     println!("Reading {}", file_path);
 
@@ -36,9 +96,11 @@ fn main() {
         Err(e) => panic!("Read file: {}", e),
     };
 
-    for line in contents.split("\n") {
-        let parts = numbers(line);
-        println!("{}", line);
-        println!("{:?}", parts);
-    }
+    let lines = contents.split("\n").collect();
+
+    let sum_one = part_one(&lines);
+    let sum_two = part_two(&lines);
+
+    println!("PartOne: {sum_one}");
+    println!("PartTwo: {sum_two}");
 }
